7746129e565f57a03bcba71a187583e08f692303
Masato Umakoshi <umakoshi@nlp.ist.i.kyoto-u.ac.jp>
2021-11-17 11:29:01 +0900

    Add base graph-bert modules

---
+++ src/models/modules/graph_bert.py
@@ -0,0 +1,44 @@
+import torch
+from transformers.models.bert.modeling_bert import BertPooler, BertPreTrainedModel
+
+from .graph_bert_layers import GraphBertEmbeddings, GraphBertEncoder
+
+BertLayerNorm = torch.nn.LayerNorm
+
+
+class MethodGraphBert(BertPreTrainedModel):
+    def __init__(self, config):
+        super(MethodGraphBert, self).__init__(config)
+        self.config = config
+
+        self.embeddings = GraphBertEmbeddings(config)
+        self.encoder = GraphBertEncoder(config)
+        self.pooler = BertPooler(config)
+
+        self.init_weights()
+
+    def get_input_embeddings(self):
+        return self.embeddings.raw_feature_embeddings
+
+    def set_input_embeddings(self, value):
+        self.embeddings.raw_feature_embeddings = value
+
+    def _prune_heads(self, heads_to_prune):
+        for layer, heads in heads_to_prune.items():
+            self.encoder.layer[layer].attention.prune_heads(heads)
+
+    def forward(self, raw_features, wl_role_ids, init_pos_ids, hop_dis_ids, head_mask=None, residual_h=None):
+        if head_mask is None:
+            head_mask = [None] * self.config.num_hidden_layers
+
+        embedding_output = self.embeddings(
+            raw_features=raw_features, wl_role_ids=wl_role_ids, init_pos_ids=init_pos_ids, hop_dis_ids=hop_dis_ids
+        )
+        encoder_outputs = self.encoder(embedding_output, head_mask=head_mask, residual_h=residual_h)
+        sequence_output = encoder_outputs[0]
+        pooled_output = self.pooler(sequence_output)
+        outputs = (
+            sequence_output,
+            pooled_output,
+        ) + encoder_outputs[1:]
+        return outputs
