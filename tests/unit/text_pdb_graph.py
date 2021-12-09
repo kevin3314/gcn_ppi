d046f79a9c545d7b648742bc48f8030b824e12d3
Masato Umakoshi <umakoshi@nlp.ist.i.kyoto-u.ac.jp>
2021-12-09 12:27:30 +0900

    Add test about load pdb

--- tests/unit/text_pdb_graph.py
+++ tests/unit/text_pdb_graph.py
@@ -1,4 +1,4 @@
-from preprocess_pdb import get_adjacency_matrix
+from preprocess_pdb import get_adjacency_matrix, get_names_coordinates_for_pdbs
 
 
 def test_get_adjacency_matrix():
@@ -21,3 +21,9 @@ def test_get_adjacency_matrix():
     assert contact_map[0, 2] == 0
     assert contact_map[0, 3] == 0
     assert contact_map[0, 4] == 0
+
+
+def test_get_names_coordinates_for_pdbs():
+    pdbs = ["1a02"]
+    pdb_root = "tests/data/pdb/"
+    residue_names, residue_coordinates = get_names_coordinates_for_pdbs(pdbs, pdb_root)
