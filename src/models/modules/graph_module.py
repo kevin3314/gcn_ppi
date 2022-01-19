81dec32930db450f2970a3aac4d0db189d1dfb3d
Masato Umakoshi <umakoshi@nlp.ist.i.kyoto-u.ac.jp>
2022-01-19 15:11:07 +0900

    Add graph model

---
+++ src/models/modules/graph_module.py
@@ -0,0 +1,32 @@
+import logging
+
+import torch.nn
+
+from .graph_modality_model import GraphModalityModel
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class GraphModel(torch.nn.Module):
+    """Monomodal model of graph.
+    Graph neural network is dedicated for graph module.
+    """
+
+    def __init__(
+        self,
+        amino_vocab_size: int,
+        node_dim: int,
+        num_gnn_layers: int,
+    ):
+        super(GraphModel, self).__init__()
+        self.gnn = GraphModalityModel(amino_vocab_size, node_dim, num_gnn_layers)
+        self.linear = torch.nn.Linear(node_dim, 1)
+        self.dropout = torch.nn.Dropout(self.text_model.encoder.config.hidden_dropout_prob)
+
+    def forward(self, data0, data1):
+        node0 = self.gnn(data0.x, data0.edge_index, data0.batch)  # (b, node_dim)
+        node1 = self.gnn(data1.x, data1.edge_index, data1.batch)  # (b, node_dim)
+        hidden_state = torch.cat([node0, node1], dim=-1)  # (b, 2*node_dim)
+        logit = self.linear(self.dropout(hidden_state))
+        return torch.squeeze(logit, dim=-1)
